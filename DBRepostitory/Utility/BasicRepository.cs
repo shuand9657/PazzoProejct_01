9a5e234404660a56a60e7736d13a37996b484f09
Andy <shuandyou6204@gmail.com>
2019-05-29 12:59:49 +0800

    增加basicRepository

---
+++ DBRepostitory/Utility/BasicRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBRepostitory.Utility
+{
+    public class BasicRepository<T> where T : class
+    {
+    }
+}
