e061b67e04cb4769e0dec3721e090a39c1b7866a
Andy <shuandyou6204@gmail.com>
2019-05-29 12:08:11 +0800

    增加customer respository 增加CustomerRepository Controller and its view

---
+++ PazzoProject_01/Controllers/CustomerRepositoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBRepostitory.Implement;
+using DBRepostitory.Models;
+using PazzoProject_01.Utility;
+
+namespace PazzoProject_01.Controllers
+{
+    public class CustomerRepositoryController : BaseController
+    {
+        private CustomerRepository<Customers> _res = null;
+        public CustomerRepositoryController()
+        {
+            _res = new CustomerRepository<Customers>();
+            
+        }
+        // GET: CustomerRepository
+        public ActionResult Index()
+        {
+            var customers = _res.Selects().AsQueryable();
+
+            List<Customers> result = customers.OrderBy(x => x.CustomerID).ToList();
+
+            return View(result);
+        }
+    }
+}
