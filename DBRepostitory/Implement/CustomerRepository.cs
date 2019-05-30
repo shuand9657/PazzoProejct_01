b41ce3f37465750216ece207159d08a068faa396
Andy <shuandyou6204@gmail.com>
2019-05-30 17:03:07 +0800

    將customer repository移到 basicRepository

--- DBRepostitory/Implement/CustomerRepository.cs
+++ DBRepostitory/Implement/CustomerRepository.cs
@@ -1,39 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using DBRepostitory.Models;
+﻿using DBRepostitory.Models;
+using DBRepostitory.Utility;
 
 namespace DBRepostitory.Implement
 {
     //repository without interface
-    public class CustomerRepository<T> where T : Customers
+    public class CustomerRepository<T> :BasicRepository<T> where T : Customers
     {
-        private NorthwindEntities db = new NorthwindEntities();
-
-        public void Create(T entity)
-        {
-            db.Set<T>().Add(entity);
-        }
-
-        /// <summary>
-        /// select one by condition
-        /// </summary>
-        /// <param name="predicate">condition</param>
-        /// <returns>return 1 items</returns>
-        public T Select(Expression<Func<T, bool>> predicate)
-        {
-            return db.Set<T>().Where(predicate).FirstOrDefault();
-        }
-        /// <summary>
-        /// Select All
-        /// </summary>
-        /// <returns>table as queryable</returns>
-        public IQueryable<T> Selects()
-        {
-            return db.Set<T>().AsQueryable();
-        }
+        
     }
 }
--- DBRepostitory/Utility/BasicRepository.cs
+++ DBRepostitory/Utility/BasicRepository.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using DBRepostitory.Models;
 
 namespace DBRepostitory.Utility
 {
     public class BasicRepository<T> where T : class
     {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        public void Create(T entity)
+        {
+            db.Set<T>().Add(entity);
+        }
+
+        /// <summary>
+        /// select one by condition
+        /// </summary>
+        /// <param name="predicate">condition</param>
+        /// <returns>return 1 items</returns>
+        public T Select(Expression<Func<T, bool>> predicate)
+        {
+            return db.Set<T>().Where(predicate).FirstOrDefault();
+        }
+        /// <summary>
+        /// Select All
+        /// </summary>
+        /// <returns>table as queryable</returns>
+        public IQueryable<T> Selects()
+        {
+            return db.Set<T>().AsQueryable();
+        }
     }
 }
